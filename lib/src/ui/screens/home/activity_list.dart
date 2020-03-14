a540745de8f0c774b0c47fdbee72e0c509ba5b6a
Josh Smith <josh@joshsmith.codes>
2020-03-13 20:19:14 -0700

    Add timeline UI

--- lib/src/ui/screens/home/activity_list.dart
+++ lib/src/ui/screens/home/activity_list.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:timeline_list/timeline.dart';
+import 'package:timeline_list/timeline_model.dart';
 
 import 'package:coronavirus_diary/src/data/models/event.dart';
 import 'activity_cards/index.dart';
@@ -17,13 +19,13 @@ class ActivityList extends StatefulWidget {
 class _ActivityListState extends State<ActivityList> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return Timeline.builder(
       itemCount: widget.events.length,
       itemBuilder: (BuildContext context, int index) {
         final Event event = widget.events[index];
         switch (event.runtimeType) {
           case LocationEvent:
-            return LocationCard(event);
+            return TimelineModel(LocationCard(event));
           default:
             return null;
         }
