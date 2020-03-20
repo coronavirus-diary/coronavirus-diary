512283ec652588335702dc9ce5252d3a710cc535
Josh Smith <josh@joshsmith.codes>
2020-03-20 16:33:07 -0700

    Fix delay issues

--- lib/src/data/repositories/checkups.dart
+++ lib/src/data/repositories/checkups.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 import 'dart:math';
 
 import 'package:coronavirus_diary/src/data/models/assessments.dart';
@@ -7,17 +6,17 @@ import 'package:coronavirus_diary/src/data/models/checkups.dart';
 
 class CheckupsRepository {
   Future<Checkup> createCheckup(Checkup checkup) async {
-    await sleep(Duration(seconds: 1));
+    await Future.delayed(Duration(seconds: 1));
     return checkup;
   }
 
   Future<Checkup> updateCheckup(Checkup updatedCheckup) async {
-    await sleep(Duration(seconds: 1));
+    await Future.delayed(Duration(seconds: 1));
     return updatedCheckup;
   }
 
   Future<Assessment> completeCheckup(String id) async {
-    await sleep(Duration(seconds: 1));
+    await Future.delayed(Duration(seconds: 1));
     return Assessment(
       processed: DateTime.now(),
       matchesPuiSymptoms: Random().nextBool(),
