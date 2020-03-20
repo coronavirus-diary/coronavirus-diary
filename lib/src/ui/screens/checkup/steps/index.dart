c7c98f463beafa3d4deb0949711d5ca64ce6cdd6
Josh Smith <josh@joshsmith.codes>
2020-03-19 17:59:22 -0700

    Rename vitals to temperature step

--- lib/src/ui/screens/checkup/steps/index.dart
+++ lib/src/ui/screens/checkup/steps/index.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'intro.dart';
 import 'subjective.dart';
-import 'vitals.dart';
+import 'temperature.dart';
 
 abstract class CheckupStep extends Widget {}
 
 final List<CheckupStep> steps = [
   IntroStep(),
   SubjectiveStep(),
-  VitalsStep(),
+  TemperatureStep(),
 ];
