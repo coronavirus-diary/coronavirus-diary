68640f7d1ae45acfa947d212545f55d30c3efad0
Josh Smith <josh@joshsmith.codes>
2020-03-20 23:35:46 -0700

    Fill out first tutorial step

--- lib/src/ui/screens/tutorial/steps/intro.dart
+++ lib/src/ui/screens/tutorial/steps/intro.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+
+import 'package:coronavirus_diary/src/ui/widgets/tutorial_step.dart';
 
 class IntroStep extends StatelessWidget {
   @override
@@ -12,18 +15,69 @@ class IntroStep extends StatelessWidget {
             padding: EdgeInsets.symmetric(horizontal: 40),
             margin: EdgeInsets.only(bottom: 20),
             child: FaIcon(
-              FontAwesomeIcons.questionCircle,
+              FontAwesomeIcons.bookMedical,
               color: Colors.white,
               size: 80,
             ),
           ),
           Container(
             padding: EdgeInsets.symmetric(horizontal: 40),
-            margin: EdgeInsets.only(bottom: 20),
+            margin: EdgeInsets.only(bottom: 40),
             child: Text(
-              "Concerned about your health?",
+              "Welcome to Coronavirus Diary",
               textAlign: TextAlign.center,
               style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            margin: EdgeInsets.only(bottom: 40),
+            child: Column(
+              children: <Widget>[
+                TutorialStep(
+                  text: 'Complete a daily health checkup.',
+                  icon: FaIcon(
+                    FontAwesomeIcons.check,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  leadingBackgroundColor: Colors.white.withOpacity(0.2),
+                  leadingPadding: EdgeInsets.all(20),
+                ),
+                TutorialStep(
+                  text: 'Receive a personalized health assessment.',
+                  icon: FaIcon(
+                    FontAwesomeIcons.heartbeat,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  leadingBackgroundColor: Colors.white.withOpacity(0.2),
+                  leadingPadding: EdgeInsets.all(20),
+                ),
+                TutorialStep(
+                  text: 'Aid COVID-19 response efforts.',
+                  icon: FaIcon(
+                    FontAwesomeIcons.handHoldingHeart,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  leadingBackgroundColor: Colors.white.withOpacity(0.2),
+                  leadingPadding: EdgeInsets.all(20),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            margin: EdgeInsets.only(bottom: 40),
+            child: RaisedButton(
+              onPressed: () => {
+                Provider.of<PageController>(context, listen: false).nextPage(
+                  duration: Duration(milliseconds: 400),
+                  curve: Curves.easeInOut,
+                )
+              },
+              child: Text('Click here to learn more'),
             ),
           ),
         ],
--- lib/src/ui/widgets/tutorial_step.dart
+++ lib/src/ui/widgets/tutorial_step.dart
@@ -4,34 +4,46 @@ class TutorialStep extends StatelessWidget {
   final String text;
   final int number;
   final Widget icon;
+  final EdgeInsetsGeometry leadingPadding;
+  final Color leadingBackgroundColor;
+  final Color numberColor;
+  final Color textColor;
 
   const TutorialStep({
     this.text,
     this.number,
     this.icon,
+    this.leadingPadding,
+    this.leadingBackgroundColor,
+    this.numberColor,
+    this.textColor,
   });
 
   Widget _getLeading(BuildContext context) {
+    if (icon == null && number == null) return null;
+
+    Widget leadingChild;
     if (icon != null) {
-      return icon;
+      leadingChild = icon;
     } else if (number != null) {
-      return Container(
-        margin: EdgeInsets.only(right: 15),
-        padding: EdgeInsets.all(12),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Theme.of(context).primaryColor,
-        ),
-        child: Text(
-          number.toString(),
-          style: TextStyle(
-            fontSize: 14,
-          ),
+      leadingChild = Text(
+        number.toString(),
+        style: TextStyle(
+          fontSize: 14,
+          color: numberColor,
         ),
       );
-    } else {
-      return null;
     }
+
+    return Container(
+      margin: EdgeInsets.only(right: 15),
+      padding: leadingPadding ?? EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        color: leadingBackgroundColor ?? Theme.of(context).primaryColor,
+      ),
+      child: leadingChild,
+    );
   }
 
   @override
@@ -39,17 +51,15 @@ class TutorialStep extends StatelessWidget {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 10),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           _getLeading(context),
           Flexible(
-            child: Padding(
-              padding: EdgeInsets.only(top: 8),
-              child: Text(
-                text,
-                style: TextStyle(
-                  fontSize: 18,
-                ),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 18,
+                color: textColor,
               ),
             ),
           ),
