2d170ad98e8c9e5d0662c48421b59cdf36dabc15
Josh Smith <josh@joshsmith.codes>
2020-03-20 19:15:05 -0700

    Save last assessment

--- lib/src/data/models/preferences.dart
+++ lib/src/data/models/preferences.dart
@@ -2,23 +2,32 @@ import 'package:json_annotation/json_annotation.dart';
 import 'package:uuid/uuid.dart';
 import 'package:uuid/uuid_util.dart';
 
+import 'assessments.dart';
+
 part 'preferences.g.dart';
 
 @JsonSerializable()
 class Preferences {
   final String userId;
+  final Assessment lastAssessment;
 
   Preferences({
     userId,
+    this.lastAssessment,
   }) : userId = userId ??
             Uuid().v4(options: {
               'grng': UuidUtil.cryptoRNG,
             });
 
-  Preferences.clone(Preferences preferences)
-      : this(
-          userId: preferences.userId,
-        );
+  Preferences cloneWith({
+    Preferences preferences,
+    Assessment lastAssessment,
+  }) {
+    return Preferences(
+      userId: preferences.userId,
+      lastAssessment: lastAssessment ?? preferences.lastAssessment,
+    );
+  }
 
   factory Preferences.fromJson(Map<String, dynamic> json) =>
       _$PreferencesFromJson(json);
