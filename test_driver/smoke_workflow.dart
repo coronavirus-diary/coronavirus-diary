f0d24da3b73d1fce092a44faffeb2507694d59cb
Jonah Williams <jonahwilliams@google.com>
2020-03-23 13:11:50 -0700

    [testing] add smoke driver test for basic workflow (#64)

---
+++ test_driver/smoke_workflow.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_driver/driver_extension.dart';
+
+import 'package:covidnearme/main.dart' as entrypoint;
+
+void main() {
+  enableFlutterDriverExtension();
+  entrypoint.main();
+}
