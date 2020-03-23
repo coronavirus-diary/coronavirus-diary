9c246a639feced5e5afb5e1de89baf3de0f6e21a
Jonah Williams <jonahwilliams@google.com>
2020-03-23 12:12:58 -0700

    [testing] add unit tests for SliderQuestion (#58)

--- lib/src/ui/widgets/questions/question_item.dart
+++ lib/src/ui/widgets/questions/question_item.dart
@@ -57,7 +57,7 @@ class _QuestionItemState extends State<QuestionItem> {
           ),
           if (widget.question.subtitle != null)
             Padding(
-              padding: EdgeInsets.only(top: 20),
+              padding: const EdgeInsets.only(top: 20),
               child: Text(
                 widget.question.subtitle,
                 textAlign: TextAlign.center,
