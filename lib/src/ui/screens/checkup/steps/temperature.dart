18fe3a7cd943ac7ac1edeb65b367468916059de5
Ben Konyi <bkonyi@google.com>
2020-03-22 18:01:59 -0700

    Fix issue where padding was applied outside of the scroll view on the temperature page
    Fixes https://github.com/coronavirus-diary/coronavirus-diary/issues/25

--- lib/src/ui/screens/checkup/steps/temperature.dart
+++ lib/src/ui/screens/checkup/steps/temperature.dart
@@ -145,9 +145,9 @@ class _TemperatureStepState extends State<TemperatureStep> {
           (VitalsResponse response) => response.id == 'temperature',
           orElse: () => null,
         );
-        return Padding(
-          padding: EdgeInsets.all(40),
-          child: ScrollableBody(
+        return ScrollableBody(
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
