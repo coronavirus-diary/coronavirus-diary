b9d890bcb10291278d8dd1c06a9f471756cd5c43
Jose Alba Aguado <jaguado@google.com>
2020-03-24 16:41:35 -0400

    You ca not press the back button to get out of the tutorial page

--- lib/src/ui/screens/tutorial/tutorial.dart
+++ lib/src/ui/screens/tutorial/tutorial.dart
@@ -31,20 +31,22 @@ class _TutorialScreenState extends State<TutorialScreen> {
   Widget build(BuildContext context) {
     return BlocBuilder<PreferencesBloc, PreferencesState>(
       builder: (context, state) {
-        return Scaffold(
-          body: ChangeNotifierProvider<PageController>.value(
-            value: _pageController,
-            child: PageView(
-              physics: NeverScrollableScrollPhysics(),
-              controller: _pageController,
-              children: <Widget>[
-                IntroStep(),
-                ConsentStep(),
-                if (state.preferences.agreedToTerms == true) GetStartedStep(),
-                if (state.preferences.agreedToTerms == false) DeniedConsent(),
-              ],
+        return WillPopScope(
+          child: Scaffold(
+            body: ChangeNotifierProvider<PageController>.value(
+              value: _pageController,
+              child: PageView(
+                physics: NeverScrollableScrollPhysics(),
+                controller: _pageController,
+                children: <Widget>[
+                  IntroStep(),
+                  ConsentStep(),
+                  if (state.preferences.agreedToTerms == true) GetStartedStep(),
+                  if (state.preferences.agreedToTerms == false) DeniedConsent(),
+                ],
+              ),
             ),
-          ),
+          ), onWillPop: () => Future.value(false),
         );
       },
     );
