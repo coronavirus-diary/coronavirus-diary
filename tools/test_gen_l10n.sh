6dc5253dea58a456e0a9b4b7bd299c9e399aa565
Christopher Fujino <christopherfujino@gmail.com>
2020-03-24 12:25:14 -0700

    [testing] Add test for gen_l10n (#109)
    * add test_gen_l10n.sh script
    
    * remove comments

---
+++ tools/test_gen_l10n.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+# This should be set on codemagic
+if [ -z "$FLUTTER_ROOT" ]; then
+  echo '$FLUTTER_ROOT env var is not set!'
+  exit 1
+fi
+
+REPO_ROOT="$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")"
+
+DART="$FLUTTER_ROOT/bin/cache/dart-sdk/bin/dart"
+DARTFMT="$FLUTTER_ROOT/bin/cache/dart-sdk/bin/dartfmt"
+GEN_L10N="$REPO_ROOT/tools/gen_l10n.dart"
+ARB_DIR="$REPO_ROOT/lib/src/l10n"
+
+# Generate localizations
+"$DART" "$GEN_L10N" --arb-dir "$ARB_DIR"
+
+# Format localizations
+"$DARTFMT" -w "$ARB_DIR"
+
+# Check if running this script introduced a change since HEAD
+if ! git diff --quiet HEAD "$ARB_DIR"; then
+  git diff HEAD "$ARB_DIR"
+  echo -e "\ntest_gen_l10n test failed!"
+  exit 1
+fi
+
+echo 'test_gen_l10n test succeeded.'
