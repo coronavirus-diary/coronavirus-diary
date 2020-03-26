a82da154f1050b306c721fec33720047685e0c8b
Jose Alba Aguado <jaguado@google.com>
2020-03-26 14:26:36 -0400

    Added semantic-label

--- lib/src/ui/screens/home/home.dart
+++ lib/src/ui/screens/home/home.dart
@@ -157,7 +157,7 @@ class _HomeScreenState extends State<HomeScreen> {
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
-            title: Text('CovidNearMe'),
+            title: Text('CovidNearMe', semanticsLabel: 'Covid Near Me',),
             leading: kReleaseMode
                 ? null
                 : IconButton(
