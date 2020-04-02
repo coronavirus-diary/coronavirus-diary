aa036e14083672a1d122616b86b7bd4e9c360a5c
Greg Spencer <gspencer@google.com>
2020-04-02 14:48:25 -0700

    Add a description and links to the website.

--- lib/src/ui/screens/home/home.dart
+++ lib/src/ui/screens/home/home.dart
@@ -1,16 +1,18 @@
-import 'package:covidnearme/src/ui/screens/symptom_report/symptom_report.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:covidnearme/src/blocs/preferences/preferences.dart';
 import 'package:covidnearme/src/l10n/app_localizations.dart';
+import 'package:covidnearme/src/ui/screens/symptom_report/symptom_report.dart';
 import 'package:covidnearme/src/ui/widgets/network_unavailable_banner.dart';
 import 'package:covidnearme/src/ui/widgets/scrollable_body.dart';
 import 'package:covidnearme/src/ui/widgets/share.dart';
 import 'package:package_info/package_info.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -137,12 +139,27 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
+class _LinkTextSpan extends TextSpan {
+  _LinkTextSpan({TextStyle style, String url, String text})
+      : super(
+            style: style,
+            text: text ?? url,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                launch(url, forceSafariVC: false);
+              });
+}
+
 class _MainMenu extends StatelessWidget {
   static const String copyright = '© 2020 Josh Smith';
 
   @override
   Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context);
+    final ThemeData themeData = Theme.of(context);
+    final TextStyle aboutTextStyle = themeData.textTheme.body2;
+    final TextStyle linkStyle =
+        themeData.textTheme.body2.copyWith(color: Color(0xff0000ff));
+    final AppLocalizations localizations = AppLocalizations.of(context);
     return PopupMenuButton(
       itemBuilder: (BuildContext context) {
         return <PopupMenuItem<String>>[
@@ -158,16 +175,66 @@ class _MainMenu extends StatelessWidget {
       onSelected: (String selection) async {
         assert(selection == 'About');
         PackageInfo packageInfo = await PackageInfo.fromPlatform();
-        showAboutDialog(
+        await showDialog<void>(
           context: context,
-          applicationVersion:
-              '${packageInfo.version}.${packageInfo.buildNumber}',
-          applicationLegalese: copyright,
-          applicationIcon: Image(
-            width: 50,
-            height: 50,
-            image: AssetImage('assets/images/icon.png'),
-          ),
+          useRootNavigator: true,
+          builder: (BuildContext context) {
+            return Theme(
+              data: themeData.copyWith(
+                buttonTheme: themeData.buttonTheme.copyWith(
+                  colorScheme: themeData.colorScheme,
+                ),
+              ),
+              child: AboutDialog(
+                applicationVersion:
+                    '${packageInfo.version}.${packageInfo.buildNumber}',
+                applicationLegalese: copyright,
+                applicationIcon: Image(
+                  width: 50,
+                  height: 50,
+                  image: AssetImage('assets/images/icon.png'),
+                ),
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0),
+                    child: RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                            style: aboutTextStyle,
+                            text:
+                                'An open source project from the Coronavirus Diary '
+                                'community. Learn more about the Coronavirus Diary '
+                                'community at ',
+                          ),
+                          _LinkTextSpan(
+                            style: linkStyle,
+                            url: 'https://covidnearme.org',
+                          ),
+                          TextSpan(
+                            style: aboutTextStyle,
+                            text:
+                                '.\n\nTo view the source code for this application, '
+                                'please visit the ',
+                          ),
+                          _LinkTextSpan(
+                            style: linkStyle,
+                            url:
+                                'https://github.com/coronavirus-diary/coronavirus-diary',
+                            text: 'coronavirus-diary github repo',
+                          ),
+                          TextSpan(
+                            style: aboutTextStyle,
+                            text: '.',
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         );
       },
       icon: Icon(Icons.more_vert),
