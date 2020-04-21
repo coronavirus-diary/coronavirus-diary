51e931b655acd4e56533df1df74f5d0f4562bfa7
Josh Smith <josh@joshsmith.codes>
2020-04-20 18:04:13 -0700

    Refactor theme

--- lib/src/ui/assets/theme.dart
+++ lib/src/ui/assets/theme.dart
@@ -1,129 +1,89 @@
 import 'package:flutter/material.dart';
 
-const Color _primaryColor = Color(0xff003a68);
-const Color _accentColor = Color(0xff00dfb7);
-const Color _textColor = _primaryColor;
-
-final ThemeData lightTheme = ThemeData.light().copyWith(
-  appBarTheme: AppBarTheme(
-    color: _primaryColor,
-  ),
-  dialogTheme: DialogTheme(
-    titleTextStyle: TextStyle(color: _textColor),
-    contentTextStyle: TextStyle(color: Colors.black),
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-  ),
-  scaffoldBackgroundColor: Colors.white,
-  primaryColor: _primaryColor,
-  backgroundColor: Colors.white,
-  accentColor: _accentColor,
-  iconTheme: ThemeData.light().iconTheme.copyWith(color: _primaryColor),
-  inputDecorationTheme: InputDecorationTheme(
-    labelStyle: TextStyle(color: _textColor),
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(2),
-      borderSide: BorderSide(
-        color: Colors.black,
-      ),
+ThemeData getTheme(
+  ThemeData baseTheme, {
+  Color primaryColor,
+  Color accentColor,
+  Color textColor,
+  Color secondaryForegroundColor,
+  Color backgroundColor,
+}) {
+  return baseTheme.copyWith(
+    appBarTheme: AppBarTheme(
+      color: primaryColor,
     ),
-    enabledBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(2),
-      borderSide: BorderSide(
-        color: Colors.black.withOpacity(0.5),
-      ),
+    dialogTheme: DialogTheme(
+      titleTextStyle: TextStyle(color: textColor),
+      contentTextStyle: TextStyle(color: secondaryForegroundColor),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
     ),
-    focusedBorder: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(2),
-      borderSide: BorderSide(
-        color: Colors.black,
-      ),
-    ),
-    contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-    errorStyle: TextStyle(
-      color: _textColor,
-      fontSize: 16,
-    ),
-  ),
-  textSelectionColor: _accentColor,
-  textSelectionHandleColor: _accentColor,
-  textTheme: ThemeData.light().textTheme,
-  primaryTextTheme: ThemeData.light().primaryTextTheme,
-  accentTextTheme: ThemeData.light().accentTextTheme,
-  colorScheme: ThemeData.light().colorScheme.copyWith(
-        primary: _primaryColor,
-        secondary: _accentColor,
-      ),
-  buttonTheme: ThemeData.light().buttonTheme.copyWith(
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-        buttonColor: _primaryColor,
-        textTheme: ButtonTextTheme.primary,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(2),
-          side: BorderSide.none,
+    scaffoldBackgroundColor: backgroundColor,
+    primaryColor: primaryColor,
+    backgroundColor: backgroundColor,
+    accentColor: accentColor,
+    iconTheme: baseTheme.iconTheme.copyWith(color: primaryColor),
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: TextStyle(color: textColor),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(2),
+        borderSide: BorderSide(
+          color: secondaryForegroundColor,
         ),
       ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(2),
+        borderSide: BorderSide(
+          color: secondaryForegroundColor.withOpacity(0.5),
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(2),
+        borderSide: BorderSide(
+          color: secondaryForegroundColor,
+        ),
+      ),
+      contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+      errorStyle: TextStyle(
+        color: textColor,
+        fontSize: 16,
+      ),
+    ),
+    textSelectionColor: accentColor,
+    textSelectionHandleColor: accentColor,
+    textTheme: baseTheme.textTheme,
+    primaryTextTheme: baseTheme.primaryTextTheme,
+    accentTextTheme: baseTheme.accentTextTheme,
+    colorScheme: baseTheme.colorScheme.copyWith(
+      primary: primaryColor,
+      secondary: accentColor,
+      background: backgroundColor,
+    ),
+    buttonTheme: baseTheme.buttonTheme.copyWith(
+      padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+      buttonColor: primaryColor,
+      textTheme: ButtonTextTheme.primary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(2),
+        side: BorderSide.none,
+      ),
+    ),
+  );
+}
+
+final ThemeData lightTheme = getTheme(
+  ThemeData.light(),
+  primaryColor: Color(0xff003a68),
+  accentColor: Color(0xff00dfb7),
+  textColor: Color(0xff003a68),
+  secondaryForegroundColor: Color(0xff000000),
+  backgroundColor: Color(0xffffffff),
 );
 
-final ThemeData darkTheme = lightTheme.copyWith(
-  colorScheme: lightTheme.colorScheme.copyWith(
-    primary: Colors.white,
-    background: lightTheme.colorScheme.primary,
-  ),
-  scaffoldBackgroundColor: lightTheme.colorScheme.primary,
-  buttonTheme: lightTheme.buttonTheme.copyWith(
-    buttonColor: lightTheme.accentColor,
-  ),
-  iconTheme: lightTheme.iconTheme.copyWith(
-    color: Colors.white,
-  ),
-  textTheme: lightTheme.textTheme.copyWith(
-    caption: lightTheme.textTheme.caption.copyWith(
-      color: Colors.white,
-    ),
-    button: lightTheme.textTheme.button.copyWith(
-      color: Colors.white,
-    ),
-    headline: lightTheme.textTheme.headline.copyWith(
-      color: Colors.white,
-    ),
-    title: lightTheme.textTheme.title.copyWith(
-      color: Colors.white,
-    ),
-    subhead: lightTheme.textTheme.subhead.copyWith(
-      color: Colors.white,
-    ),
-    body1: lightTheme.textTheme.body1.copyWith(
-      color: Colors.white,
-    ),
-    body2: lightTheme.textTheme.body2.copyWith(
-      color: Colors.white,
-    ),
-    display1: lightTheme.textTheme.display1.copyWith(
-      color: Colors.white,
-    ),
-    display2: lightTheme.textTheme.display2.copyWith(
-      color: Colors.white,
-    ),
-    display3: lightTheme.textTheme.display3.copyWith(
-      color: Colors.white,
-    ),
-    display4: lightTheme.textTheme.display4.copyWith(
-      color: Colors.white,
-    ),
-  ),
-  inputDecorationTheme: lightTheme.inputDecorationTheme.copyWith(
-    border: lightTheme.inputDecorationTheme.border.copyWith(
-        borderSide: lightTheme.inputDecorationTheme.border.borderSide
-            .copyWith(color: Colors.white)),
-    enabledBorder: lightTheme.inputDecorationTheme.enabledBorder.copyWith(
-        borderSide: lightTheme.inputDecorationTheme.enabledBorder.borderSide
-            .copyWith(color: Colors.white.withOpacity(0.5))),
-    focusedBorder: lightTheme.inputDecorationTheme.focusedBorder.copyWith(
-        borderSide: lightTheme.inputDecorationTheme.focusedBorder.borderSide
-            .copyWith(color: Colors.white)),
-    focusColor: Colors.white,
-    labelStyle: lightTheme.inputDecorationTheme.labelStyle.copyWith(
-      color: Colors.white,
-    ),
-  ),
+final ThemeData darkTheme = getTheme(
+  ThemeData.dark(),
+  primaryColor: Color(0xffffffff),
+  accentColor: Color(0xff00dfb7),
+  textColor: Color(0xffffffff),
+  secondaryForegroundColor: Color(0xffffffff),
+  backgroundColor: Color(0xff003a68),
 );
